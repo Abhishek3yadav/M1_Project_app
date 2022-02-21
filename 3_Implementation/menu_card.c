d6ef3b83bbd3a0dea5fcd41b5fee20a82cbc7bc0
Abhishek3yadav <98915922+Abhishek3yadav@users.noreply.github.com>
2022-02-21 19:58:17 +0530

    Update menu_card.c

--- 3_Implementation/menu_card.c
+++ 3_Implementation/menu_card.c
@@ -1,93 +1,192 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 
- void Breakfast(){
-    printf(" WELCOME TO THE RAJDHANI HOTEL! \n");
+int invoiceGenerated=0;
+int ordersProcessed=0;
 
-    printf(" ALOO PARATHA                 =40Rs\n");
-    printf(" PANEER PARATHA               =45Rs\n");
-    printf(" CHHOLE BHATURE               =50Rs\n");
-    
+ struct items{
+     char item[50];
+     float price;
+     int qty;
+ };
+ struct orders{
+     char customer[50];
+     char date[50];
+     int numofitems;
+     float total;
+     int invoiceId;
+     struct items itm[50];
+ };
 
+struct orders ord[50];
 
-}
-void Lunch()
-{
-   printf(" WELCOME TO THE RAJDHANI HOTEL! \n");
-   
-   printf("   RAJDHANI THALI SPECIAL            = 140Rs\n");
-   printf("   FULL PLATE BIRYANI                = 100Rs\n");
-   printf("   CHICKEN MASALA                    = 80Rs\n");
-   printf("   EGG CURRY WITH CHAPATI            = 70Rs\n");
-   printf("   VEG SPECIAL THALI                 = 100Rs\n");
-
-}void Dinner ()
-{
-   printf(" WELCOME TO THE RAJDHANI HOTEL! \n");
-
-   printf("  RAJDHANI THALI SPECIAL            = 140Rs\n");
-   printf("  FULL PLATE BIRYANI                = 100Rs\n");
-   printf("  CHICKEN MASALA                    = 80Rs\n");
-   printf("  EGG CURRY WITH CHAPATI            = 70Rs\n");
-   printf("  VEG SPECIAL THALI                 = 100Rs\n");
-
-
-}
-void Soft_Drink ()
-{
-    printf("   WELCOME TO THE RAJDHANI HOTEL! \n");
-   
-    printf("   COFFEE              = 50Rs\n");
-    printf("   TANDORI TEA         = 40Rs\n");
-    printf("    MILK SAKE          = 45Rs\n");
-    printf("    COLD COFFEE        = 45Rs\n");
-}
-
-int main()
-{
-    char Choice;
-    int Carry;
-    int qty,rate,total;
-    do{
-        printf("Enter your choice\n");
-        printf("Enter B for Breakfast\n");
-        printf("Enter L FOR Lunch\n");
-        printf("Enter D for Dinner\n");
-        printf("Enter S for Soft Drink\n");
-        printf("For online delivery call 999888777 \n");
-        scanf("%c",&Choice);
-        switch(Choice){
-            case 'B':
-            Breakfast();
-            break;
-            case 'L':
-            Lunch();
-            break;
-            case 'D':
-            Dinner();
-            break;
-            case 'S':
-           
-            Soft_Drink();
-            printf(" YOU HAVE ENTER SOFT DRINK \n COFFEE");
-            scanf("%d",&qty);
-            rate=50;
-            total=qty*rate ;
-            printf("\n total amount :%dRs",total);
-            break;
-            printf("\n YOU HAVE ENTER SOFT DRINK \n TEA");
-            scanf("%d",&qty);
-            rate=40;
-            total=qty*rate ;
-            printf("\n total amount :%dRs",total);
-            
-            break;
-            default:
-            printf("Invalid Choice");
-        }
  
-        printf("\n Enter 0 for exit \n 1 for Continue");
-        scanf("%d",&Carry);
+ int getInvoiceId(){
+   return invoiceGenerated;
+ }
 
-    }while(Carry!=0);
-    return 0;
+void showBillheader(char name[50],char date[50],int orderIndex){
+    printf("\n\n");
+    printf("\t       RAJDHANI RESTURANT");
+    printf("\n\t     --------------------");
+    printf("\nDate:%s",date);
+    printf("\nInvoice To: %s", name);
+    printf("\nInvoice Id:%d",ord[orderIndex].invoiceId);
+    printf("\n");
+    printf("----------------------------------------------------\n");
+    printf("Items\t\t");
+    printf("Qty\t\t");
+    printf("Total\t\t");
+    printf("\n----------------------------------------------------");
+    printf("\n\n");
 }
+ void showBillbody(char item[50],int qty,float price){
+      printf("%s\t\t",item);
+       printf("%d\t\t",qty);
+        printf("%.2f\t\t",qty*price);
+         printf("\n");
+ }
+ 
+ float getDiscount(float total,float discountPercent){
+     float discount = ((100-discountPercent)/100)*total;
+     return discount;
+ }
+ 
+ float getGSTPrice(float netTotal){
+     float gstSlab = 0.09;
+     float gst = gstSlab * netTotal;
+     return gst;
+ }
+ 
+ float getGrandTotal(float netTotal,float gstPrice){
+     return netTotal+ (2*gstPrice);
+ }
+ 
+ void showBillFooter(float total){
+      printf("\n");
+      float discount = 10;
+      float netTotal = getDiscount(total,discount);
+      float gst = getGSTPrice(netTotal);
+      float grandTotal = getGrandTotal(netTotal,gst);
+       printf("----------------------------------------------------");
+       printf("\nSub Total\t\t\t%.2f",total);
+        printf("\nDiscount @10%s\t\t\t%.2f","%",total-netTotal);
+        printf("\n\t\t\t\t--------");
+        printf("\nNetTotal\t\t\t%.2f",netTotal);
+        printf("\nCGST @9%s\t\t\t%.2f","%",gst);
+        printf("\nSGST @9%s\t\t\t%.2f","%",gst);
+        printf("\n----------------------------------------------------");
+        printf("\nGrand Total\t\t\t%.2f\n",grandTotal);
+ }
+
+ 
+ void createInvoice(){
+     
+     printf("\nPlease enter the name of the customer::");
+         fgets(ord[ordersProcessed].customer,50,stdin);
+         ord[ordersProcessed].customer[strlen(ord[ordersProcessed].customer)-1] =0;
+         strcpy(ord[ordersProcessed].date,__DATE__);
+         printf("\nPlease enter the number of items::");
+         scanf("%d",&ord[ordersProcessed].numofitems);
+         ord[ordersProcessed].invoiceId = getInvoiceId();
+
+         for(int i=0;i<ord[ordersProcessed].numofitems;i++){
+             fgetc(stdin);
+             printf("\n\n");
+             printf("please enter the item %d::",i+1);
+             fgets(ord[ordersProcessed].itm[i].item,20,stdin);
+             ord[ordersProcessed].itm[i].item[strlen(ord[ordersProcessed].itm[i].item)-1]=0;
+             printf("\n please enter the quantity::");
+             scanf("%d",&ord[ordersProcessed].itm[i].qty);
+             printf("\n please enter the unit price::");
+             scanf("%f",&ord[ordersProcessed].itm[i].price);
+
+         }
+         
+        showBillheader(ord[ordersProcessed].customer,__DATE__,ordersProcessed);
+         int total = 0 ;
+         for(int i=0;i<ord[ordersProcessed].numofitems;++i){
+             total += (ord[ordersProcessed].itm[i].qty * ord[ordersProcessed].itm[i].price);
+             showBillbody(ord[ordersProcessed].itm[i].item,ord[ordersProcessed].itm[i].qty,ord[ordersProcessed].itm[i].price);
+         }
+         
+        showBillFooter(total);
+
+ }
+ 
+ 
+ void showAllInvoices(){
+
+    for(int orderIndex=0;orderIndex<ordersProcessed;++orderIndex){
+        showBillheader(ord[orderIndex].customer,__DATE__,orderIndex);
+         int total = 0 ;
+         for(int itemIndex=0;itemIndex<ord[orderIndex].numofitems;++itemIndex){
+             total += (ord[orderIndex].itm[itemIndex].qty * ord[orderIndex].itm[itemIndex].price);
+             showBillbody(ord[orderIndex].itm[itemIndex].item,ord[orderIndex].itm[itemIndex].qty,ord[orderIndex].itm[itemIndex].price);
+         }
+        showBillFooter(total);
+    }
+ }
+
+ void searchInvoiceById(){
+     int invoiceId;
+     printf("\nEnter Invoice Id::");
+     scanf("%d",&invoiceId);
+     int invoiceFound = 0;
+
+     for(int orderIndex=0;orderIndex<ordersProcessed;++orderIndex){
+         if(ord[orderIndex].invoiceId == invoiceId){
+            invoiceFound = 1; 
+            showBillheader(ord[orderIndex].customer,__DATE__,orderIndex);
+            int total = 0 ;
+            for(int itemIndex=0;itemIndex<ord[orderIndex].numofitems;++itemIndex){
+                total += (ord[orderIndex].itm[itemIndex].qty * ord[orderIndex].itm[itemIndex].price);
+                showBillbody(ord[orderIndex].itm[itemIndex].item,ord[orderIndex].itm[itemIndex].qty,ord[orderIndex].itm[itemIndex].price);
+            }
+            showBillFooter(total);
+         }
+     }
+
+     if(invoiceFound == 0){
+        printf("\nNo Invoice with this Invoice Id\n\n");
+     }
+ }
+ 
+ int main(){
+    int opt;
+    
+    do{
+     // DASHBOARD
+     printf("\n\t       RAJDHANI RESTURANT         ");
+     printf("\n1.Generate Invoice");
+     printf("\n2.Show all Invoices");
+     printf("\n3.Search Invoice");
+     printf("\n4.Exit");
+
+     printf("\n\nPlease select your prefered operation:\t");
+     scanf("%d",&opt);
+     
+     fgetc(stdin);
+     switch(opt){
+         
+        case 1: invoiceGenerated++;
+                createInvoice();
+                ordersProcessed++;
+                //printf("\norderProcessed::%d",ordersProcessed);
+                break;
+                 
+        case 2: showAllInvoices(); 
+                break;
+        case 3: searchInvoiceById();
+                break;                
+         
+     }
+    }while(opt!=4);
+          
+          printf("\n\n");
+     
+
+     return 0;
+      
+ }
